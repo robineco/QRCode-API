1a999bd0db3a97121bfae21c06aa06d0a5d28546
robineco <robin.schlund@outlook.de>
2019-12-30 20:52:27 +0100

    initial commit

---
+++ src/main/java/rocks/aereo/qrcodeapi/QrcodeApiApplication.java
@@ -0,0 +1,13 @@
+package rocks.aereo.qrcodeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrcodeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rocks/aereo/qrcodeapi/QrcodeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.aereo.qrcodeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
